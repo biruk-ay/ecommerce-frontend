ac9771ae022947aded55a63b79eb978bc96fdd16
biruk-ay <birukindustry@gmail.com>
2024-11-25 07:01:39 +0300

    Implemented Auth State

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthContextProvider } from './auth/state/AuthCtxProvider';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
