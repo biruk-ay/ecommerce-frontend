8dd2a17a14b9e030f63700d38d3a2e3da51c7cdd
biruk-ay <birukindustry@gmail.com>
2024-11-27 03:51:00 +0300

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
-import './App.css'
+import "./App.css";
+import { Route, Routes, Navigate, useLocation } from "react-router-dom";
+import AuthContext from "./auth/state/AuthCtxProvider";
+import { useContext } from "react";
+import Auth from "./components/Auth";
 
 function App() {
+  const { authState } = useContext(AuthContext);
+  const location = useLocation();
 
   return (
-          <h1 className="text-center text-3xl font-bold underline">Hello World</h1>
-  )
+    <div className="App">
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Navigate
+              to={authState.isLoggedIn ? location.pathname : "/user/login"}
+            />
+          }
+        />
+        {!authState.isLoggedIn && (
+          <Route path="user">
+            <Route path="register" element={<Auth />} />
+            <Route path="login" element={<Auth />} />
+          </Route>
+        )}
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
