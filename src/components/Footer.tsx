cfdca395f20265b91703cab6112a9b78c44ea161
biruk-ay <birukindustry@gmail.com>
2024-12-08 03:55:29 +0300

    Made footer responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,13 @@
 const Footer = () => {
   return (
-    <footer className="bg-primary flex fixed bottom-0 mt-1 justify-evenly items-center w-full text-white font-roboto">
-      <div className="flex-col">
-        <a href="/" className="">
-          <img className="h-11 w-11 ml-3" src="../../logo.png" alt="Company Logo"></img>
+    <footer className="bg-primary flex flex-col sm:flex-row fixed bottom-0 mt-1 justify-evenly items-center w-full text-white font-roboto">
+      <div className="flex-col mr-80 mt-2 sm:mr-0 sm:mt-0">
+        <a href="/">
+          <img className="flex justify-start sm:self-auto h-11 w-11 ml-3" src="../../logo.png" alt="Company Logo"></img>
           <div className="font-cursive font-semibold text-2xl">gebeya</div>
         </a>
       </div>
-      <div className="flex-col self-start">
+      <div className="flex-col self-start mb-4 ml-2">
         <div className="font-semibold text-xl mb-2">Contact Us</div>
         <ul className="list-disc pl-8">
             <li>Phone Number: +251 917171717</li>
@@ -15,7 +15,7 @@ const Footer = () => {
             <li>Telegram: t.me/gebeya</li>
         </ul>
       </div>
-      <div className="flex-col self-start mb-4">
+      <div className="flex-col self-start mb-4 ml-2">
         <div className="font-semibold text-xl mb-2">Popular Catagories</div>
         <ul className="list-disc pl-8">
             <li>Shoes</li>
@@ -25,7 +25,7 @@ const Footer = () => {
             <li>Clothes</li>
         </ul>
       </div>
-      <div className="flex-col self-start">
+      <div className="flex-col self-start mb-4 ml-2">
         <div className="font-semibold text-xl mb-2">Customer Services</div>
         <ul className="list-disc pl-8">
             <li>About Us</li>
