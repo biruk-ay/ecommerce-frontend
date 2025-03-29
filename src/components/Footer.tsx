9968dd87b7efb916e8de51f64289463d0959e1fa
biruk-ay <birukindustry@gmail.com>
2025-03-29 07:52:02 +0300

    sm view updated for footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,17 @@
 const Footer = () => {
   return (
-    <footer className="bg-primary flex flex-col sm:flex-row bottom-0 mt-1 justify-evenly items-center w-full text-white font-roboto">
-      <div className="flex-col mr-80 mt-2 sm:mr-0 sm:mt-0">
+    <footer className="bg-primary flex flex-col sm:flex-row bottom-0 mt-1 justify-center sm:justify-evenly items-center w-full text-white font-roboto">
+      <div className="flex flex-col items-center">
         <a href="/">
           <img
-            className="flex justify-start sm:self-auto h-11 w-11 ml-3"
+            className="flex justify-center sm:self-auto h-11 w-11 ml-3"
             src="../../logo.png"
             alt="Company Logo"
           ></img>
           <div className="font-cursive font-semibold text-2xl">gebeya</div>
         </a>
       </div>
-      <div className="flex-col self-start mb-4 ml-2">
+      <div className="flex-col justify-center sm:self-start mb-4 ml-24 sm:ml-2">
         <div className="font-semibold text-xl mb-2">Contact Us</div>
         <ul className="list-disc pl-8">
           <li>Phone Number: +251 917171717</li>
@@ -19,7 +19,7 @@ const Footer = () => {
           <li>Telegram: t.me/gebeya</li>
         </ul>
       </div>
-      <div className="flex-col self-start mb-4 ml-2">
+      <div className="flex-col justify-center sm:self-start mb-4 ml-2">
         <div className="font-semibold text-xl mb-2">Popular Catagories</div>
         <ul className="list-disc pl-8">
           <li>Shoes</li>
@@ -29,7 +29,7 @@ const Footer = () => {
           <li>Clothes</li>
         </ul>
       </div>
-      <div className="flex-col self-start mb-4 ml-2">
+      <div className="flex-col justify-center sm:self-start mb-4 ml-2">
         <div className="font-semibold text-xl mb-2">Customer Services</div>
         <ul className="list-disc pl-8">
           <li>About Us</li>
